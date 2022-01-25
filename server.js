234f70fa20ee5e9836948def846b2657743bcd80
Rob van Daal <rvandaal@hotmail.com>
2022-01-25 19:01:56 +0100

    Added simple bitvavo query and enabled CORS

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
+
+const { bitvavo } = require('./bitvavo-api');
+
+const corsOptions = {
+    origin: 'http://localhost:4200', // portnumber has to be included
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/api/test', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavo.assets({});
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// start the server listening for requests
+app.listen(
+    process.env.PORT || 3000,
+    () => console.log("Server is running...")
+);
 
