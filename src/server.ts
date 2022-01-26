700a9b00c35cf9bf1b2cbc98cf19d6cd00664c33
Rob van Daal <rvandaal@hotmail.com>
2022-01-26 02:22:55 +0100

    Added endpoints

--- src/server.ts
+++ src/server.ts
@@ -33,6 +33,60 @@ app.get('/api/assets', cors(corsOptions), async function (req, res) {
     }
 });
 
+app.get('/api/balance/:symbol?', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getBalance(req.params.symbol);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/api/tickerprices/:market?', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getTickerPrices(req.params.market);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/api/tickerprices24h', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getTickerPrices24h();
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/api/candles/:market/:interval', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getCandles(req.params.market, req.params.interval);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/api/trades/:market', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getTrades(req.params.market);
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.get('/api/openorders', cors(corsOptions), async function (req, res) {
+    try {
+        const response = await bitvavoService.getOpenOrders();
+        return res.json(response);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 // start the server listening for requests
 app.listen(
     process.env.PORT || 3000,
